668bbfc3b16647f87a8fd5752162a3ac58029d6d
York-xia <1208209000@qq.com>
2020-09-17 22:01:26 +0800

    add req url const

--- app/bridage/constant/const.go
+++ app/bridage/constant/const.go
@@ -1,1 +1,61 @@
 package constant
+
+const (
+	EXPEIRE_ACCOUNT_CODE   = 2000 //用户账户登录过期状态码
+	EXPEIRE_WXACCOUNT_CODE = 2001 //微信登录状态过期状态码(暂且不用)
+)
+
+const (
+	BASE_URL = "49.234.86.244:8080" //微信对接baselink
+)
+
+// login
+const (
+	LOGIN_AUTO_URL   = BASE_URL + "/login/auto"
+	LOGIN_AWAKE_URL  = BASE_URL + "/login/awake"
+	LOGIN_CHECK_URL  = BASE_URL + "/login/check"
+	LOGIN_HEART_URL  = BASE_URL + "/login/heartbeat"
+	LOGIN_INIT_URL   = BASE_URL + "/login/init"
+	LOGIN_LOGOUT_URL = BASE_URL + "/login/logout"
+	LOGIN_QRCODE_URL = BASE_URL + "/login/qr_code"
+)
+
+// wxuser
+const (
+	WXUSER_PROFILE_URL = BASE_URL + "/user/profile"
+)
+
+//contact
+const (
+	CONTACT_ACCEPT_URL     = BASE_URL + "/contact/accept"
+	CONTACT_ADD_URL        = BASE_URL + "/contact/add"
+	CONTACT_BATCH_URL      = BASE_URL + "/contact/batch"
+	CONTACT_LIST_URL       = BASE_URL + "/contact/list/all"
+	CONTACT_GROUP_LIST_URL = BASE_URL + "/contact/list/group"
+	CONTACT_SEARCH_URL     = BASE_URL + "/contact/search"
+)
+
+// group
+const (
+	GROUP_ACCEPT_URL      = BASE_URL + "/group/accept"
+	GROUP_CREATE_URL      = BASE_URL + "/group/create"
+	GROUP_DETAIL_URL      = BASE_URL + "/group/get/detail"
+	GROUP_INFO_URL        = BASE_URL + "/group/get/info"
+	GROUP_MEMBERS_URL     = BASE_URL + "/group/get/members"
+	GROUP_ADD_MEMBERS_URL = BASE_URL + "/group/members/add"
+	GROUP_DEL_MEMBERS_URL = BASE_URL + "/group/members/delete"
+	// GROUP_ADD_MEMBERS_URL  = BASE_URL + "/group/members/invite"
+	GROUP_QUIT_URL         = BASE_URL + "/group/quit"
+	GROUP_SET_ANNOUNCE_URL = BASE_URL + "/group/set/announcement"
+)
+
+// label
+const (
+	LABEL_ADD_URL         = BASE_URL + "/label/add"
+	LABEL_DELETE_URL      = BASE_URL + "/label/delete"
+	LABEL_LIST_URL        = BASE_URL + "/label/list"
+	LABEL_UPDATE_URL      = BASE_URL + "/label/update"
+	LABEL_LIST_UPDATE_URL = BASE_URL + "/label/update/list"
+)
+
+// sns

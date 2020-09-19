0cc19e8ee472ba4647e137c15c2a4362ff5fd3f3
York-xia <1208209000@qq.com>
2020-09-19 23:00:47 +0800

    后台管理员表

---
+++ app/bridage/models/manager.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/astaxie/beego/orm"
+)
+
+// Manager ...
+type Manager struct {
+	ID       int64  `orm:"auto;column(id)"`
+	IsAdmin  bool   `orm:"column(is_admin);default(1)"`
+	Account  string `orm:"size(50);column(account)" `
+	PassWord string `orm:"size(50);column(pwd)"`
+	NickName string `orm:"size(50);column(nickname)"`
+	Tel      string `orm:"size(50);column(tel)"`
+	Avatar   string `orm:"size(100);column(avatar)"`
+}
+
+func init() {
+	orm.RegisterModel(new(Manager))
+}

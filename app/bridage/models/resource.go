222d019674c87cd6c927f2fe6422fcb5cd6ac7b0
York-xia <1208209000@qq.com>
2020-09-29 22:19:09 +0800

    简化表结构

--- app/bridage/models/resource.go
+++ app/bridage/models/resource.go
@@ -7,10 +7,11 @@ import (
 // Resource send source
 // 素材库
 type Resource struct {
-	ID   int64  `orm:"auto;column(id)"`
-	Type int    `orm:"column(type)"`            //消息内容类型
-	Text string `orm:"type(text)"`              //消息文本
-	Data string `orm:"size(300); column(data)"` //其他类型的地址(后期可拆)
+	ID       int64  `orm:"auto;column(id)"`              //
+	Type     int    `orm:"column(type)"`                 //消息内容类型(0:文本 1:图片....)
+	IsPublic int    `orm:"column(is_public);default(0)"` //资源是否是公有（区分素材公共库展示和增加方案功能的具体回复内容）
+	Data     string `orm:"size(300); column(data)"`      //类型的具体内容(后期可拆)
+	Manager  string `orm:"size(30);column(manager)"`     //属于哪个用户的素材库(用户Tel)
 }
 
 func init() {
--- app/bridage/models/welcome.go
+++ app/bridage/models/welcome.go
@@ -6,23 +6,14 @@ import (
 
 // Welcome ...
 type Welcome struct {
-	ID                  int64            `orm:"auto;column(id)"`                           //
-	Switch              bool             `orm:"column(switch);default(1)"`                 // 功能总开关
-	WaitSeconds         int              `orm:"column(waitseconds)"`                       // 等待时间
-	WaitTimeSwitch      bool             `orm:"column(wait_time_switch);default(1)"`       // 进群多少秒发送
-	WaitNewersNumSwitch int              `orm:"column(wait_newers_num_switch);default(1)"` // 新进群多少人发送
-	ReplyContents       []*SourceWelcome `orm:"reverse(many)"`                             // 空 前端默认展示
-	Resources           string           `orm:"size(300); column(resouces)"`               // 从素材库导入的回复内容(ids, ","连接)
-}
-
-// SourceWelcome 欢迎语的回复内容(可以多个)
-type SourceWelcome struct {
-	ID      int64    `orm:"auto;column(id)"`
-	Type    int      `orm:"column(type)"`               //消息内容类型(0:文本 1:图片....)
-	Content string   `orm:"size(300); column(content)"` //回复内容
-	Welcome *Welcome `orm:"rel(fk)"`                    //
+	ID                  int64  `orm:"auto;column(id)"`                           //
+	Switch              bool   `orm:"column(switch);default(1)"`                 // 功能总开关
+	WaitSeconds         int    `orm:"column(waitseconds)"`                       // 等待时间
+	WaitTimeSwitch      bool   `orm:"column(wait_time_switch);default(1)"`       // 进群多少秒发送
+	WaitNewersNumSwitch int    `orm:"column(wait_newers_num_switch);default(1)"` // 新进群多少人发送
+	Resources           string `orm:"size(300); column(resouces)"`               // 来自资源库的具体回复内容(ids, ","连接,有多个)
 }
 
 func init() {
-	orm.RegisterModel(new(Welcome), new(SourceWelcome))
+	orm.RegisterModel(new(Welcome))
 }

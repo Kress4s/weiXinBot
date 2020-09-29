6149006ce13bc37ce7fc57e4ec1826ff64ecee63
York-xia <1208209000@qq.com>
2020-09-29 21:53:01 +0800

    入群欢迎语表结构修改

--- app/bridage/models/grouplan.go
+++ app/bridage/models/grouplan.go
@@ -4,12 +4,11 @@ import "github.com/astaxie/beego/orm"
 
 // GroupPlan ...
 type GroupPlan struct {
-	ID   int64  `orm:"auto;column(id)"`       //
-	Name string `orm:"size(50);column(name)"` //
-	// Type    int      `orm:"column(type)"`          // 群管类型(0:欢迎新人；1:关键字新人... )
-	PlanIds string   `orm:"column(plan_id)"` // 群管类型的IDs(用“，”连接)
-	Manager *Manager `orm:"rel(fk)"`         //属于哪个用户创建的群管方案
-	Groups  []*Group `orm:"reverse(many)"`   //
+	ID      int64    `orm:"auto;column(id)"`       //
+	Name    string   `orm:"size(50);column(name)"` // 名称
+	PlanIds string   `orm:"column(plan_id)"`       // 群管类型的IDs(用“，”连接) 接口加个type参数，指定id的位置对应关系，点的是哪个配置的id（0:欢迎新人；1:关键字新人...）
+	Manager *Manager `orm:"rel(fk)"`               // 属于哪个用户创建的群管方案
+	Groups  []*Group `orm:"reverse(many)"`         //
 }
 
 func init() {
--- app/bridage/models/welcome.go
+++ app/bridage/models/welcome.go
@@ -6,16 +6,23 @@ import (
 
 // Welcome ...
 type Welcome struct {
-	ID                  int64  `orm:"auto;column(id)"`                           //
-	Switch              bool   `orm:"column(switch);default(1)"`                 //功能总开关
-	Words               string `orm:"size(200);column(words)"`                   //回复语
-	Type                int    `orm:"column(type);default(0)"`                   //回复类型
-	WaitSeconds         int    `orm:"column(waitseconds)"`                       //等待时间
-	WaitTimeSwitch      bool   `orm:"column(wait_time_switch);default(1)"`       //进群多少秒发送
-	WaitNewersNumSwitch int    `orm:"column(wait_newers_num_switch);default(1)"` //新进群多少人发送
-	SourceURL           string `orm:"size(300); column(source_url)"`             //回复文件地址
+	ID                  int64            `orm:"auto;column(id)"`                           //
+	Switch              bool             `orm:"column(switch);default(1)"`                 // 功能总开关
+	WaitSeconds         int              `orm:"column(waitseconds)"`                       // 等待时间
+	WaitTimeSwitch      bool             `orm:"column(wait_time_switch);default(1)"`       // 进群多少秒发送
+	WaitNewersNumSwitch int              `orm:"column(wait_newers_num_switch);default(1)"` // 新进群多少人发送
+	ReplyContents       []*SourceWelcome `orm:"reverse(many)"`                             // 空 前端默认展示
+	Resources           string           `orm:"size(300); column(resouces)"`               // 从素材库导入的回复内容(ids, ","连接)
+}
+
+// SourceWelcome 欢迎语的回复内容(可以多个)
+type SourceWelcome struct {
+	ID      int64    `orm:"auto;column(id)"`
+	Type    int      `orm:"column(type)"`               //消息内容类型(0:文本 1:图片....)
+	Content string   `orm:"size(300); column(content)"` //回复内容
+	Welcome *Welcome `orm:"rel(fk)"`                    //
 }
 
 func init() {
-	orm.RegisterModel(new(Welcome))
+	orm.RegisterModel(new(Welcome), new(SourceWelcome))
 }
